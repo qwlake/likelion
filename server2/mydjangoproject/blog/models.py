4da52d67af3961f96354830b822c6d8035a81d9e
JungWoo <qwlake@gmail.com>
2019-05-14 03:34:36 +0900

    edit server2

--- server2/mydjangoproject/blog/models.py
+++ server2/mydjangoproject/blog/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
