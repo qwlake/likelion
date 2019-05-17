7a0b38fad27d19a7b431639a61dfdf3cead79cb8
qwlake <qwlake@gmail.com>
2019-05-17 17:57:25 +0900

    add imageview

--- server3/blog/urls.py
+++ server3/blog/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-import blog.views
-import imageview.views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('', blog.views.index, name='index'),
-    path('imageview/', imageview.views.imageview, name="imageview"),
+    path('admin', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
+    path('new', views.new, name="new"),
+    path('create', views.create, name="create"),
+    path('modify/<int:post_id>/', views.modify, name="modify"),
+    path('update/<int:post_id>/', views.update, name="update"),
+    path('delete/<int:post_id>/', views.delete, name="delete"),
+    path('newpost', views.newpost, name="newpost"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
