ee8e8e9f389791d730e8d8f01f8cd02cf6d9ddcf
JungWoo <qwlake@gmail.com>
2019-05-21 00:06:25 +0900

    fixed error

--- server-django/server/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jungwoo\github\likelion-session\server-django\server\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- server2/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\qwlak\github\likelion\server2\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
